39babf336c07630b266ac829c4e60b42edb680c3
VIGOVSKYI <vuv3000@ukr.net>
2023-03-24 18:23:19 +0200

    finale

---
+++ index.js
@@ -0,0 +1,54 @@
+const contacts = require("./db/contacts.js");
+
+const { Command } = require("commander");
+const program = new Command();
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
+
+// program.parse(process.argv);
+
+// const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+      break;
+
+    case "get":
+      const getById = await contacts.getContactById(id);
+      return console.table(getById);
+      break;
+
+    case "add":
+      const addNewContact = await contacts.addContact({ name, email, phone });
+      return console.table(addNewContact);
+      break;
+
+    case "remove":
+      const removeById = await contacts.removeContact(id);
+      return console.table(removeById);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAction(argv);
